a2f8150e6263c9c766f6b612905277eb14ea0600
Ioan Calin Borcoman <iborco@gmail.com>
2023-01-18 12:50:19 +0200

    removed helpers for duration and file size as they are specific to another application; added extracting date from yaml

--- src/qtyamlcppadapter/yamlhelper.cpp
+++ src/qtyamlcppadapter/yamlhelper.cpp
@@ -66,24 +66,6 @@ quint64 quint64FromYaml(const YAML::Node &node, const char *key, quint64 default
     return node[key] ? node[key].as<quint64>() : defaultValue;
 }
 
-quint64 fileSizeFromYaml(const YAML::Node &node, const char *key, quint64 defaultValue)
-{
-    try {
-        return quint64FromYaml(node, key, defaultValue);
-    } catch (...) {
-        return parsedFileSize(stringFromYaml(node, key));
-    }
-}
-
-quint64 durationFromYaml(const YAML::Node &node, const char *key, quint64 defaultValue)
-{
-    try {
-        return quint64FromYaml(node, key, defaultValue);
-    } catch (...) {
-        return parsedDuration(stringFromYaml(node, key));
-    }
-}
-
 QString stringFromYaml(const YAML::Node &node, const char *key, const QString &defaultValue)
 {
     return node[key] ? QString::fromStdString(node[key].as<std::string>()) : defaultValue;
@@ -123,103 +105,13 @@ QStringList stringListFromYaml(const YAML::Node &node, const char *key, bool *ne
     return string_list;
 }
 
-QString formattedDuration(quint64 duration)
+QDateTime dateTimeFromYaml(const YAML::Node &node, const char *key, const QDateTime &defaultValue)
 {
-    if (duration == 0) {
-        return QString();
-    }
+    if (!node[key])
+        return defaultValue;
 
-    const quint64 minute = 60;
-    const quint64 hour = 60 * minute;
-
-    quint64 hours = duration / hour;
-    quint64 minutes = (duration % hour) / minute;
-    quint64 seconds = duration % minute;
-
-    QStringList repr;
-
-    if (hours) {
-        repr.append(QString("%1h").arg(hours));
-    }
-    if (minutes) {
-        repr.append(QString("%1m").arg(minutes));
-    }
-    if (seconds && hours == 0) {
-        repr.append(QString("%1s").arg(seconds));
-    }
-
-    return repr.join(" ");
-}
-
-quint64 parsedDuration(const QString &value)
-{
-    auto tokens = value.split(" ");
-    quint64 duration = 0UL;
-    const quint64 minute = 60UL;
-    const quint64 hour = 60 * minute;
-
-    for (const auto& token: tokens) {
-        if (token.length() >= 1) {
-            auto suffix = token.right(1);
-            auto value = token.first(token.length() - 1).toULongLong();
-
-            if (suffix == "h") {
-                duration += value * hour;
-            } else if (suffix == "m") {
-                duration += value * minute;
-            } else if (suffix == "s") {
-                duration += value;
-            }
-        }
-    }
-
-//    qDebug() << "Parsed string duration:" << value << "->" << duration << "s";
-
-    return duration;
-}
-
-QString formattedFileSize(quint64 size)
-{
-    if (size == 0) {
-        return QString();
-    }
-
-    const char* suffix[]{"B", "KB", "MB", "GB", "TB", "PB", "EB"};
-    const quint64 base{1000UL}; // use 1000 to get same results as macOS Finder
-    int i{0};
-    double mantissa = size;
-    for (; std::ceil(mantissa) >= base && i < sizeof(suffix); mantissa /= base, ++i) { }
-    mantissa = std::ceil(mantissa * 100.) / 100.;
-    return QString("%1 %2").arg(mantissa, 0, 'f', 2).arg(suffix[i]);
-}
-
-quint64 parsedFileSize(const QString &value)
-{
-    auto values = value.split(" ");
-    auto raw_size = values[0].toDouble();
-    QString suffix = values.length() > 1 ? values[1] : "B";
-    quint64 size {0UL};
-    const quint64 base {1000UL}; // use 1000 to get same results as macOS Finder
-
-    if (suffix == "B") {
-        size = raw_size;
-    } else if (suffix == "KB") {
-        size = raw_size * base;
-    } else if (suffix == "MB") {
-        size = raw_size *  base * base;
-    } else if (suffix == "GB") {
-        size = raw_size *  base * base * base;
-    } else if (suffix == "TB") {
-        size = raw_size *  base * base * base * base;
-    } else if (suffix == "PB") {
-        size = raw_size *  base * base * base * base * base;
-    } else if (suffix == "EB") {
-        size = raw_size *  base * base * base * base * base * base;
-    }
-
-//    qDebug() << "Parsed file size:" << value << "->" << size;
-
-    return size;
+    auto repr = QString::fromStdString(node[key].as<std::string>());
+    return QDateTime::fromString(repr, Qt::ISODate);
 }
 
 }
--- src/qtyamlcppadapter/yamlhelper.h
+++ src/qtyamlcppadapter/yamlhelper.h
@@ -4,18 +4,17 @@
 
 #pragma once
 
+#include <QDateTime>
 #include <QStringList>
 
 #include "enumhelper.h"
-#include "yaml-cpp/emitter.h"
+
+namespace YAML {
+class Emitter;
+class Node;
+}
 
 namespace qtyamlcppadapter {
-
-QString formattedDuration(quint64 duration);
-quint64 parsedDuration(const QString& value);
-
-QString formattedFileSize(quint64 size);
-quint64 parsedFileSize(const QString& size);
 
 void emitYaml(YAML::Emitter& out, const char* key, const QString& value);
 void emitYaml(YAML::Emitter& out, const char* key, bool value);
@@ -27,10 +26,9 @@ void emitYamlLiteral(YAML::Emitter& out, const char* key, const QString& value);
 bool boolFromYaml(const YAML::Node& node, const char* key, bool defaultValue = false);
 int intFromYaml(const YAML::Node& node, const char* key, int defaultValue = 0);
 quint64 quint64FromYaml(const YAML::Node &node, const char *key, quint64 defaultValue = 0UL);
-quint64 fileSizeFromYaml(const YAML::Node &node, const char *key, quint64 defaultValue = 0UL);
-quint64 durationFromYaml(const YAML::Node &node, const char *key, quint64 defaultValue = 0UL);
 QString stringFromYaml(const YAML::Node& node, const char* key, const QString& defaultValue = QString());
 QStringList stringListFromYaml(const YAML::Node& node, const char* key, bool *needsSave = nullptr, const QStringList& defaultValue = QStringList());
+QDateTime dateTimeFromYaml(const YAML::Node& node, const char* key, const QDateTime& defaultValue = QDateTime());
 
 template <typename E>
 E enumFromYaml(const YAML::Node &node, const char *key, E defaultValue) {
